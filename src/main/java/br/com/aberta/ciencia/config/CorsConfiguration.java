91c78fc1e8b31958896380814e1422ddf36e90b1
hoyamams <hoyamams@gmail.com>
2022-01-24 17:11:09 -0300

    cors -

--- src/main/java/br/com/aberta/ciencia/config/CorsConfiguration.java
+++ src/main/java/br/com/aberta/ciencia/config/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
              //   .allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin", "Origin", "X-Auth-Token")
             //    .allowCredentials(false)
